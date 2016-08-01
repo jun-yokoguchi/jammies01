3d811b9f60e95ef0ce298b58951f4333f6c27a37
BANCO <BANCO@BANCO-no-MacBook-Air.local>
2016-08-02 06:56:55 +0900

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module Jammies01
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+    	g.stylesheets false
+    	g.javascripts false
+    	g.helper false
+    	g.test_framwork false
+    end
   end
 end
